07cd4184b9a00e5a33d247cc6417a951449aa1fa
disverst <69248760+disverst@users.noreply.github.com>
2021-07-21 20:29:29 +0300

    first_commit
    The first application in React, with project structure and components

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import './App.css';
+
+let text = 'test variable';
+
+const style = {
+	fontSize: '24px',
+	fontStyle : 'italic',
+	color: 'red'
+}
+
+function App() {
+	let text2 = '222';
+	return (
+		<>
+		<div className="container">
+			<h1 style={style}>
+				app_1
+			</h1>
+			<img src="/images/1.webp" alt="nature" />
+			<ul>
+				<li>Hello</li>
+				<li>{text+text2}</li>
+			</ul>
+		</div>
+		</>
+	);
+}
+
+export default App;
